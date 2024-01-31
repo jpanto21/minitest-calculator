e6f5abead0c6c5e6484566e2bfffb82ff14d6abb
Jose Pantoja <77706927+jpanto21@users.noreply.github.com>
2024-01-31 22:42:17 +0000

    committed

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -1,1 +1,10 @@
-test
+# tests/test_calculator.rb
+
+require 'minitest/autorun'
+require './calculator'
+
+class TestCalculator < Minitest::Test
+  def test_addition
+    calculator = Calculator.new
+    assert_equal 4, calculator.add(2, 2), "Addition method failed"
+  end
